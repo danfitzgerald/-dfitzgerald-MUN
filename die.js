d0e0a4e74b1a8b40f43653c9727cd95333e817d0
Daniel Fitzgerald <dfitzgerald@mun.ca>
2022-11-11 17:09:16 -0330

    added rolling random die

---
+++ die.js
@@ -0,0 +1,35 @@
+const numberRotations = [
+    [-.03, -.03, 0], // Number 1
+    [-.47,.03,0],
+    [-.03,-.28,0],
+    [-.03,.22,0],
+    [-.28,0,-.03],
+    [.22,0,.03]      // Number 6
+];
+
+var dieElement = null;
+
+function rollDie(id) {
+    dieElement = document.getElementById(id);
+    let randomNumber = Math.floor((Math.random() * 6) + 1);
+
+    let transform = getCSSTransform(randomNumber);
+
+    console.log(randomNumber);
+    console.log(transform);
+
+    dieElement.style.transform = transform;
+}
+
+function getCSSTransform(randNum) {
+    // Calculate random number of turns the die will roll.
+    let xTurns = Math.floor(Math.random() * 11);
+    let yTurns = Math.floor(Math.random() * 11);
+    let zTurns = Math.floor(Math.random() * 11);
+
+    xTurns += numberRotations[randNum-1][0];
+    yTurns += numberRotations[randNum-1][1];
+    zTurns += numberRotations[randNum-1][2];
+
+    return "rotateX("+xTurns+"turn) rotateY("+yTurns+"turn) rotateZ("+zTurns+"turn)";
+}
